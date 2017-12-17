7903ec2f441ada14ddd6c517124f658628380bd2
unnerved <123pechenin@gmail.com>
2017-12-17 20:33:57 +0200

    First updated

---
+++ src/com/git/lab9/First.java
@@ -0,0 +1,11 @@
+package com.git.lab9;
+
+
+public class First {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello universe!");
+    }
+
+}
